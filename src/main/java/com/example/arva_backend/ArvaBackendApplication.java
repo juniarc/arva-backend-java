c664876a8f1dda6101b5cf8fcac977240fc717c3
Cahya Juniar <juniarcahya@gmail.com>
2025-07-09 13:31:56 +0700

    init

---
+++ src/main/java/com/example/arva_backend/ArvaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.arva_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArvaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArvaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/arva_backend/ArvaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.arva_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArvaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
